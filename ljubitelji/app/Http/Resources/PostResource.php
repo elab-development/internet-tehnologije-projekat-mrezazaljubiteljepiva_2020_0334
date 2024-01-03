b25aa854427b3f823f8755fc45f13f114238330d
kosturko123 <kosturko123@gmail.com>
2024-01-03 22:51:25 +0100

    Dodati Post i User resursi

---
+++ ljubitelji/app/Http/Resources/PostResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public static $wrap = 'post';
+    public function toArray(Request $request): array
+    {
+        //return parent::toArray($request);
+        return [
+            'id'=>$this->resource->id,
+            'text'=>$this->resource->text,
+            'post_title'=>$this->resource->post_title,
+            //'user'=>$this->resource->user
+            'user'=> new UserResource($this->resource->user)
+        ];
+    }
+}
